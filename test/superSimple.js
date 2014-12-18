235584bd7504fc4dd15b4df685b85430ca310974
Julian <jutanke@googlemail.com>
2014-12-18 07:28:32 +0100

    basic algorithm

--- test/superSimple.js
+++ test/superSimple.js
@@ -10,7 +10,9 @@ console.log("merge:", Protocol.merge(
     [{address:"a", age:2},{address:"b", age:3},{address:"c", age:1}],
     [{address:"a", age:5},{address:"b", age:11},{address:"d",age:2},{address:"e",age:6}],
     [{address:"b", age:3}],
-    3
+    4
 ));
 
 console.log("sort: ", Protocol.sort([{address:"a", age:5},{address:"b", age:11},{address:"d",age:2},{address:"e",age:6}]));
+
+console.log("stringify: " + Protocol.stringify({age:3, address: "a", peer: {demo:33, lol: 111, qq: "Hallo welt"}}));
